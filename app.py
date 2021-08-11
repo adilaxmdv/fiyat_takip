4363a1a986be4bcff1e8e702e7ca648bb83ced3a
Adil Axmedov <adilaxmedov13@gmail.com>
2021-08-11 21:56:58 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+import requests
+import smtplib
+from bs4 import BeautifulSoup
+import time
+
+url = 'Your '
+
+headers = {
+    'User-Agent': 'Your User Agent'}
+
+
+def check_price():
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find(id='product-name').get_text().strip()
+    title = title[0:17]
+    print(title)
+    span = soup.find(id='offering-price')
+    content = span.attrs.get('content')
+    price = float(content)
+    print(content)
+
+    if (price <= 210):
+        send_mail(title)
+
+
+def send_mail(title):
+    sender = 'senderMail'
+    receiver = 'recevier Mail'
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(sender, 'mail password')
+        subject = title + 'Istedigin fiyata dustu!'
+        body = 'Bu linkten Gide bilirsin =>' + url
+        mailContent = f"To:{receiver}\nFrom:{sender}\nSubject:{subject}\n\n{body}"
+        server.sendmail(sender, receiver, mailContent)
+        print('Mail Gonderildi')
+    except smtplib.SMTPException as e:
+        print(e)
+    finally:
+        server.quit()
+
+
+while (1):
+    check_price()
+    time.sleep(60 * 60)
